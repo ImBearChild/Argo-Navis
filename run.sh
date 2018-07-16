65b8a6eafd0eae58c1ff68e08bd3660dd93128ec
Copas <copas@Lenovo.bearchild>
2018-07-16 19:28:52 +0800

    Change server.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-( bluefish &)
-
-konsole --hide-menubar --hide-tabbar --hold -e "python3 -m http.server 8081"
--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
  
 #! /bin/bash
-konsole --hide-menubar --hide-tabbar --hold -e "python3 -m http.server 8081"
+konsole --hide-menubar --hide-tabbar --hold -e "python3 -m http.server 8088"
